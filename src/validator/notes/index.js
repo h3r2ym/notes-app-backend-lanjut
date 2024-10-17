a7496b71f2b754f8bd2c5c2bbaf49bf41007097c
Herry Mahardika <h3r2ym@gmail.com>
2024-10-17 09:53:43 +0800

    init app

---
+++ src/validator/notes/index.js
@@ -0,0 +1,13 @@
+const { NotePayloadSchema } = require('./schema');
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validateResult = NotePayloadSchema.validate(payload);
+
+    if (validateResult.error) {
+      throw new Error(validateResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
