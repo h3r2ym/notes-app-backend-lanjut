da92af945692a57f6a68f9a6d7a8ee065a9ffcb4
Herry Mahardika <h3r2ym@gmail.com>
2024-10-21 14:40:38 +0800

    U module users

---
+++ src/api/users/handler.js
@@ -0,0 +1,92 @@
+const autoBind = require('auto-bind');
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postNoteHandler(request, h) {
+    this._validator.validateUserPayload(request.payload);
+    const { username, password, fullname } = request.payload;
+
+    const userId = await this._service.addNote({
+      username,
+      password,
+      fullname,
+    });
+
+    const response = h.response({
+      status: 'success',
+      message: 'User berhasil ditambahkan',
+      data: {
+        userId,
+      },
+    });
+
+    response.code(201);
+
+    return response;
+  }
+
+  async getUserByIdHandler(request) {
+    const { id } = request.params;
+    const user = await this._service.getUserByIdHandler(id);
+
+    return {
+      status: 'success',
+      data: {
+        user,
+      },
+    };
+  }
+
+  // async getNotesHandler() {
+  //   const notes = await this._service.getNotes();
+  //   return {
+  //     status: 'success',
+  //     data: {
+  //       notes,
+  //     },
+  //   };
+  // }
+
+  // async getNoteByIdHandler(request) {
+  //   const { id } = request.params;
+
+  //   const note = await this._service.getNoteById(id);
+  //   return {
+  //     status: 'success',
+  //     data: {
+  //       note,
+  //     },
+  //   };
+  // }
+
+  // async putNoteByIdHandler(request) {
+  //   this._validator.validateNotePayload(request.payload);
+  //   const { id } = request.params;
+
+  //   await this._service.editNoteById(id, request.payload);
+
+  //   return {
+  //     status: 'success',
+  //     message: 'Catatan berhasil diperbarui',
+  //   };
+  // }
+
+  // async deleteNoteByIdHandler(request) {
+  //   const { id } = request.params;
+
+  //   await this._service.deleteNoteById(id);
+
+  //   return {
+  //     status: 'success',
+  //     message: 'Catatan berhasil dihapus',
+  //   };
+  // }
+}
+
+module.exports = UsersHandler;
--- src/server.js
+++ src/server.js
@@ -6,9 +6,14 @@ const NotesService = require('./services/postgres/NotesService');
 const notes = require('./api/notes');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exceptions/ClientError');
+const UsersService = require('./services/postgres/UsersService');
+const users = require('./api/users');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -19,13 +24,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
